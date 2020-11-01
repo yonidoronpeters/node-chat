f5074a640b1c34f480fdc91dcee8e598ef56f60e
Yoni Doron-Peters <jdoronpeters@gloo.us>
2020-11-01 15:12:24 -0700

    Basic websocket messaging and location sending

---
+++ public/js/chat.js
@@ -0,0 +1,21 @@
+const socket = io();
+
+socket.on('message', (msg) => {
+  console.log(msg);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit('sendMessage', message);
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', `Location: ${position.coords.latitude}, ${position.coords.longitude}`);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
+io.on('connection', (socket) => {
+  console.log('new Websocket connection');
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('sendLocation', (location) => {
+    socket.broadcast.emit('message', location);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
 });
